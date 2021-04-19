838f261d17d6acd2ede04dd5579543ff36fbb885
Laura0203107 <lcfernandez17@ucatolica.edu.co>
2021-04-18 20:27:25 -0500

    commit master

---
+++ windows.js
@@ -0,0 +1,23 @@
+var width = window.screen.width;
+var height = window.screen.height;
+
+window.document.write("<p>Ancho: " + width + " : " + "Alt: " + height + "</p>");
+
+var navegador = window.navigator.userAgent;
+window.document.write(navegador);
+
+//Redireccion a google se usa la propiedad Location Object
+window.location.href = "https://facebook.com/";
+
+
+//Metodo Open por medio de un boton
+/*function open_window(){
+    window.open("https://espanol.yahoo.com/");
+}*/
+
+
+//Abre en ventana emergente el navegador
+/*function open_window(){
+    window.open("http://www.google.com", "_blanck", "width=300, height=300");
+}*/
+
